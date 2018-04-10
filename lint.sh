3c841dff400bdcdb53551ec25958217ddf11ba54
Jesse Green <jesse.x.green@nab.com.au>
2018-04-10 19:46:16 +1000

    Adding printing to linting script

--- lint.sh
+++ lint.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
 echo "Running linting";
-echo pwd;
+pwd;
 ./node_modules/html-linter/bin/html-linter.js --config ./linting_configs/.html-linter.json "$1";
-echo pwd;
+pwd;
 ./node_modules/sass-lint/bin/sass-lint.js --config ./linting_configs/.scss-lint.yml "$2";
 ./node_modules/eslint/bin/eslint.js --config ./linting_configs/.eslintrc.json "$3";
 

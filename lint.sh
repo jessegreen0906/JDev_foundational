9948e9c6dfde8af79cd2d0ab12349bd8b8fe15f9
Jesse Green <jesse.x.green@nab.com.au>
2018-05-01 14:37:33 +1000

    Adding all scripts to linting script

--- lint.sh
+++ lint.sh
@@ -1,10 +1,8 @@
 #! /bin/bash
 
 echo "Running linting";
-pwd;
-#./node_modules/html-linter/bin/html-linter.js --config ./linting_configs/.html-linter.json "$1";
-pwd;
+./node_modules/html-linter/bin/html-linter.js --config ./linting_configs/.html-linter.json "$1";
 ./node_modules/sass-lint/bin/sass-lint.js --config ./linting_configs/.scss-lint.yml "$2";
-#./node_modules/eslint/bin/eslint.js --config ./linting_configs/.eslintrc.json "$3";
+./node_modules/eslint/bin/eslint.js --config ./linting_configs/.eslintrc.json "$3";
 
 echo "Linting complete";

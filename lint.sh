6ecde18df481af373270f8da7bcfdb493805b1ea
Jesse Green <jesse.x.green@nab.com.au>
2018-05-07 14:53:29 +1000

    lint edit

--- lint.sh
+++ lint.sh
@@ -2,9 +2,16 @@
 
 echo "Running linting";
 echo `pwd`;
-sass-lint --config ./linting_configs/.html-linter.json "$1";
-./node_modules/html-linter/bin/html-linter.js --config ./linting_configs/.html-linter.json "$1";
-./node_modules/sass-lint/bin/sass-lint.js --config ./linting_configs/.scss-lint.yml "$2";
+#sass-lint --config ./linting_configs/.html-linter.json "$1";
+
+
+#./node_modules/html-linter/bin/html-linter.js --config ./linting_configs/.html-linter.json "$1";
+
+if [[ -f ./node_modules/sass-lint/bin/sass-lint.js ]]; then
+	./node_modules/sass-lint/bin/sass-lint.js --config ./linting_configs/.scss-lint.yml "$2";
+elif [[ -f ../sass-lint/bin/sass-lint.js ]]; then
+	../sass-lint/bin/sass-lint.js --config ./linting_configs/.scss-lint.yml "$2";
+fi
 ./node_modules/eslint/bin/eslint.js --config ./linting_configs/.eslintrc.json "$3";
 
 echo "Linting complete";

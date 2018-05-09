a2113cd2467fffff9f68e786c86f9406f1b0e3ee
Jesse Green <jesse.x.green@nab.com.au>
2018-05-10 09:22:06 +1000

    Updating Jest config

--- unitTest.sh
+++ unitTest.sh
@@ -3,9 +3,9 @@
 echo "Running Unit Tests";
 
 if [[ -f ./node_modules/jest/bin/jest.js ]]; then
-	./node_modules/jest/bin/jest.js --config ./testing_configs/jest.config.json "$1";
+	./node_modules/jest/bin/jest.js --config ./testing_configs/jest.config.json;
 elif [[ -f ../jest/bin/jest.js ]]; then
-	../jest/bin/jest.js --config ./testing_configs/jest.config.json "$1";
+	../jest/bin/jest.js --config ./testing_configs/jest.config.json;
 fi
 
 echo "Unit Tests complete";

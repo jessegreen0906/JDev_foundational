c22e3b568a93a66494a402f63f437b1d2076a9ef
Jesse Green <jesse.x.green@nab.com.au>
2018-06-05 06:48:53 +1000

    Updates to phantomcss config file

--- testing_configs/phantomcss.conf.js
+++ testing_configs/phantomcss.conf.js
@@ -1,9 +1,9 @@
 var args = {
-rebase: casper.cli.get('rebase'),
+	rebase: casper.cli.get('rebase'),
 	
-	screenshotRoot: './tests/visual_tests/screenshots/baseline',
-	failedComparisonsRoot: './tests/visual_tests/screenshots/failures',
-	comparisonResultRoot: './tests/visual_tests/screenshots/baseline',
+	screenshotRoot: '../../tests/visual_tests/screenshots/baseline',
+	failedComparisonsRoot: '../../tests/visual_tests/screenshots/failures',
+	comparisonResultRoot: '../../tests/visual_tests/screenshots/baseline',
 	
 	addIteratorToImage: false,
 	cleanupComparisonImages: true,

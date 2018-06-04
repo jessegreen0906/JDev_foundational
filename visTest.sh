8162366387e49d808ac61735334f16cba2c084aa
Jesse Green <jesse.x.green@nab.com.au>
2018-06-05 07:10:30 +1000

    Updates to visual test script

--- visTest.sh
+++ visTest.sh
@@ -2,10 +2,10 @@
 
 echo "Running Visual Tests";
 
-if [[ -f ./node_modules/casperjs/bin/casperjs.js ]]; then
-	./node_modules/casperjs/bin/casperjs.js test "$1";
-elif [[ -f ../casperjs/bin/casperjs.js ]]; then
-	../casperjs/bin/casperjs.js test "$1";
+if [[ -f ./node_modules/casperjs/bin/casperjs ]]; then
+	./node_modules/casperjs/bin/casperjs test "$1";
+elif [[ -f ../casperjs/bin/casperjs ]]; then
+	../casperjs/bin/casperjs test "$1";
 fi
 
 echo "Visual Tests complete";
